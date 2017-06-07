9972c18cbfa34ce1ba4cddd10b7617ac7eeaa36f
AthulNair <athulnair@AthulNairs-MacBook-Air.local>
2017-06-07 15:08:36 +0530

    Menu

---
+++ app/src/main/java/com/example/android/slides/addVehicleDetailsActivity.java
@@ -0,0 +1,13 @@
+package com.example.android.slides;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class addVehicleDetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_vehicle_details);
+    }
+}
--- app/src/main/java/com/example/android/slides/showVehiclesActivity.java
+++ app/src/main/java/com/example/android/slides/showVehiclesActivity.java
@@ -2,26 +2,24 @@ package com.example.android.slides;
 
 import android.content.Intent;
 import android.support.v4.widget.DrawerLayout;
-import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.Toast;
 
-public class ownerHistoryActivity extends AppCompatActivity {
+public class showVehiclesActivity extends AppCompatActivity {
 
-    sessionManager session;
     private DrawerLayout drawerLayout;
     private ActionBarDrawerToggle navToggle;
     private Toolbar toolbar;
+    private sessionManager session;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_owner_history);
+        setContentView(R.layout.activity_show_vehicles);
 
         toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
@@ -45,11 +43,6 @@ public class ownerHistoryActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
-    public void addVehicle(View view) {
-//        Intent intent = new Intent(this, a)
-    }
-
-
 
     /* Menu onClick Functions */
 
@@ -57,7 +50,7 @@ public class ownerHistoryActivity extends AppCompatActivity {
     public void logout(MenuItem item){
         session = new sessionManager(this);
         session.setFirstTimeLaunch(true);
-        Intent intent = new Intent(ownerHistoryActivity.this, ownerLoginActivity.class);
+        Intent intent = new Intent(this, ownerLoginActivity.class);
         intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
         startActivity(intent);
     }
@@ -67,13 +60,5 @@ public class ownerHistoryActivity extends AppCompatActivity {
         Intent intent = new Intent(this, showVehiclesActivity.class);
         startActivity(intent);
     }
-
-    public void history(MenuItem item){
-        Intent intent = new Intent(this, ownerHistoryActivity.class);
-        startActivity(intent);
-    }
-
-
-
 
 }
